f95089f5f23f1c5e0501bb2e1bae9c2e8eb27bcc
malu <cocoimarius@gmail.com>
2015-09-12 18:27:18 +0300

    push 1

--- databasestuffs/src/main/java/com/test/model/CustomOptions.java
+++ databasestuffs/src/main/java/com/test/model/CustomOptions.java
@@ -7,9 +7,7 @@ import org.apache.commons.cli.Options;
 public class CustomOptions extends Options {
 
 	public void addOptions(Options optionsToAdd) {
-		OptionGroup og = new OptionGroup();
-		optionsToAdd.getOptions().iterator()
-		
+		System.out.println("add options");
 	}
 	
 }
